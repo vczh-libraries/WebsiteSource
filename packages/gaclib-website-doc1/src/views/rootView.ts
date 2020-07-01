b0ec4cb48b2c0e8b2028462e50548690d332522c
Zihan Chen <vczh@163.com>
2020-07-01 10:30:02 -0700

    Update rootView.ts

--- packages/gaclib-website-doc1/src/views/rootView.ts
+++ packages/gaclib-website-doc1/src/views/rootView.ts
@@ -9,7 +9,7 @@ export const viewExport = {
             <table class="ArticleTable" cellpadding="0" cellspacing="0">
                 <tr>
                     <td class="NavigateHeader" colspan="6" align="left">
-                        <img src="/logo.gif" />
+                        <img src="/doc/current/logo.gif" />
                     </td>
                 </tr>
                 <tr>
