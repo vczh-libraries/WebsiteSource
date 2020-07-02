608cd87b95a81efb02669e59eea06bc047f965ea
Zihan Chen <vczh@163.com>
2020-07-01 17:43:09 -0700

    ...

--- packages/website-doc1/src/views/directoryView.ts
+++ packages/website-doc1/src/views/directoryView.ts
@@ -1,4 +1,4 @@
-import { html, render } from 'lit-html';
+import { html, render, TemplateResult } from 'lit-html';
 
 export interface DirectoryNode {
     name: string;
@@ -12,13 +12,29 @@ export interface DirectoryInfo {
     subNodes: DirectoryNode[];
 }
 
+function renderDirectory(dinfo: DirectoryInfo, dnodes: DirectoryNode[]): TemplateResult {
+    return html`${
+        dnodes.map((dnode: DirectoryNode) => {
+            const subHtml = dnode.subNodes === undefined ? undefined : html`<div class="Children">${renderDirectory(dinfo, dnode.subNodes)}</div>`;
+            if (dnode.selected) {
+                return html`<div class="DirectoryNode Selected">${dnode.name}</div>${subHtml}`;
+            } else if (dnode.path === undefined) {
+                return html`<div class="DirectoryNode">${dnode.name}</div>${subHtml}`;
+            } else {
+                return html`<a class="DirectoryNode" href="${dinfo.pathPrefix}/${dnode.path.join('/')}.html">${dnode.name}</a>${subHtml}`;
+            }
+        })
+        }`;
+}
+
 export const viewExport = {
     renderView(model: {}, target: Element): void {
+        const dinfo = <DirectoryInfo>window['MVC-Resources.directoryInfo'];
         const htmlTemplate = html`
 <table class="DirectoryTable">
     <tr>
         <td class="TreeView" valign="top">
-            <strong>The reference directory is under construction</strong>
+            ${renderDirectory(dinfo, dinfo.subNodes)}
         </td>
         <td class="ContentView" valign="top">
             <div id="directoryViewContainer"/>
