278fbf040f9408474db4f52c1b4e0077bdf25787
Zihan Chen <vczh@163.com>
2020-07-09 13:56:39 -0700

    Update homePages.ts

--- packages/website/src/homePages.ts
+++ packages/website/src/homePages.ts
@@ -1,5 +1,5 @@
-import { litHtmlViewCallback, MvcRouter, ViewConfig } from 'gaclib-host';
-import { route } from 'gaclib-mvc';
+import { litHtmlViewCallback, litHtmlViewRouterCallback, MvcRouter, MvcRouterResult, ViewConfig } from 'gaclib-host';
+import { HttpMethods, route } from 'gaclib-mvc';
 import { loadArticle } from './topLevelPages';
 import { views } from './views';
 
@@ -13,9 +13,58 @@ const homePageConfig: ViewConfig = {
     }
 };
 
-export const homePageDynamicUrls: string[] = [];
+const categoryPages: { [key: string]: { category: string; article: string } } = {
+    gacui: {
+        category: 'GacUI',
+        article: 'home/gacui.xml'
+    },
+    'data-processing': {
+        category: 'Data',
+        article: 'home/data-processing.xml'
+    },
+    'string-processing': {
+        category: 'String',
+        article: 'home/string-processing.xml'
+    },
+    'reflection-scripting': {
+        category: 'Scripting',
+        article: 'home/reflection-processing.xml'
+    }
+};
+
+interface CategoryPageModel {
+    page: string;
+}
+
+export const homePageDynamicUrls: string[] = Object.keys(categoryPages).map((name: string) => `/home/${name}.html`);
 
 export function registerHomePages(router: MvcRouter): void {
     router.register([], route`/`, litHtmlViewCallback(views, 'Gaclib-HomeCategoryView', homePageConfig));
     router.register([], route`/index.html`, litHtmlViewCallback(views, 'Gaclib-HomeCategoryView', homePageConfig));
+
+    router.register(
+        [],
+        route`/home/${{ page: '' }}.html`,
+        (method: HttpMethods, model: CategoryPageModel): MvcRouterResult | undefined => {
+            const page = categoryPages[model.page];
+            if (page === undefined) {
+                return undefined;
+            }
+            return litHtmlViewRouterCallback(
+                method,
+                model,
+                views,
+                'Gaclib-ArticleView',
+                {
+                    info: { title: 'Gaclib -- GPU Accelerated C++ User Interface (vczh)' },
+                    embeddedResources: {
+                        activeButton: 'Home',
+                        activeCategory: page.category,
+                        homeArticle: loadArticle('home.xml'),
+                        categoryArticle: loadArticle(page.article)
+                    }
+                }
+            );
+        }
+    );
 }
