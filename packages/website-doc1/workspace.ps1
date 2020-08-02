fb97345428171bd996cac6e7e97420fa4862a162
Zihan Chen <vczh@163.com>
2020-08-01 22:53:15 -0700

    Update workspace.ps1

--- packages/website-doc1/workspace.ps1
+++ packages/website-doc1/workspace.ps1
@@ -1,3 +1,5 @@
 code .;
 ..\..\..\Release\Tutorial\Console_Workflow\Console_Workflow.sln;
 ..\..\..\Workflow\Test\UnitTest\UnitTest.sln;
+Start-Process http://localhost:8080/doc/current/workflow/lang.html;
+npm run start;
