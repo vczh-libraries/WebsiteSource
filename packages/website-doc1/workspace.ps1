f4f8a214893bdb6a381b6ae164b714386f88bc05
Zihan Chen <vczh@163.com>
2020-09-17 01:13:58 -0700

    ...

--- packages/website-doc1/workspace.ps1
+++ packages/website-doc1/workspace.ps1
@@ -1,5 +1,5 @@
 code .;
-..\..\..\Release\Tutorial\Console_Workflow\Console_Workflow.sln;
+..\..\..\Document\Tools\CppDoc\CppDoc.sln;
 ..\..\..\Workflow\Test\UnitTest\UnitTest.sln;
 Start-Process http://localhost:8080/doc/current/workflow/lang.html;
 npm run start;
