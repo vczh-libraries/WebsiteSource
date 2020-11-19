9f00ccb103438a4219d0a3cb8b201f5277826d3a
Zihan Chen <vczh@163.com>
2020-11-18 22:01:47 -0800

    kb_compositions

--- packages/website-doc1/workspace.ps1
+++ packages/website-doc1/workspace.ps1
@@ -1,4 +1,8 @@
 code .;
 ..\..\..\GacUI\Test\GacUISrc\GacUISrc.sln
+pushd ..\..\..\Release\SampleForDoc\GacUI
+.\GacUI.sln
+code .
+popd
 Start-Process http://localhost:8080/doc/current/gacui/home.html;
 npm run start;
