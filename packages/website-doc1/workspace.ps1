0b8d61c236356bc4962647bdf5348597800acf7e
Zihan Chen <vczh@163.com>
2020-12-12 18:16:38 -0800

    Begin xmlres

--- packages/website-doc1/workspace.ps1
+++ packages/website-doc1/workspace.ps1
@@ -6,5 +6,5 @@ cd SampleForDoc\GacUI
 .\GacUI.sln
 code .
 popd
-Start-Process http://localhost:8080/doc/current/gacui/home.html;
+Start-Process http://localhost:8080/doc/current/gacui/xmlres/tag_instance.html;
 npm run start;
