9e8066c3b87cce5080adb606ed77268ee4a43192
Zihan Chen <vczh@163.com>
2021-06-27 00:39:14 -0700

    Update workspace.ps1

--- packages/website-doc1/workspace.ps1
+++ packages/website-doc1/workspace.ps1
@@ -1,10 +1,6 @@
 code .;
+code ..\..\..\Release
 ..\..\..\GacUI\Test\GacUISrc\GacUISrc.sln
-pushd ..\..\..\Release
-# code .
-cd SampleForDoc\GacUI
-.\GacUI.sln
-code .
-popd
-Start-Process http://localhost:8080/doc/current/gacui/components/controls/home.html;
+..\..\..\Release\SampleForDoc\GacUI\GacUI.sln
+Start-Process http://localhost:8080/doc/current/gacui/components/controls/ribbon/home.html;
 npm run start;
