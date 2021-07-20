f7cbf27353ce1f963d74e7c36ccd0e76cb9b104f
Zihan Chen <vczh@163.com>
2021-07-19 21:44:07 -0700

    ...

--- packages/website-doc1/workspace.ps1
+++ packages/website-doc1/workspace.ps1
@@ -2,5 +2,5 @@ code .;
 code ..\..\..\Release
 ..\..\..\GacUI\Test\GacUISrc\GacUISrc.sln
 ..\..\..\Release\SampleForDoc\GacUI\GacUI.sln
-Start-Process http://localhost:8080/doc/current/gacui/components/controls/ribbon/home.html;
+Start-Process http://localhost:8080/doc/current/gacui/components/itemplates/home.html;
 npm run start;
