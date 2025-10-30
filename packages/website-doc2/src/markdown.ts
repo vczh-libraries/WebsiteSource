d7949ee201445c15c4eb5f8c9eaafdd28de09565
vczh <vczh@outlook.com>
2025-10-30 02:05:42 -0700

    Update markdown.ts

--- packages/website-doc2/src/markdown.ts
+++ packages/website-doc2/src/markdown.ts
@@ -2,16 +2,22 @@ import { DocTree, DocTreeNode } from './treeView.js';
 import * as fs from 'fs';
 import * as path from 'path';
 
-function generateIndexForProject(nodes: DocTreeNode[], prefix: string, directory: string, relativeDirectory: string): string {
+function generateIndexForProject(nodes: DocTreeNode[], prefix: string, directory: string, relativeDirectory: string, collectedNodes: { [path: string]: DocTreeNode }): string {
     let indexMd = "";
     for (const node of nodes.filter(node => node.kind === "article")) {
         const markdownRelativePath = node.path!.join('/') + '.md';
+        collectedNodes[path.join(directory, markdownRelativePath)] = node;
+
         indexMd += `${prefix}- [${node.name}](${relativeDirectory}${markdownRelativePath})\r\n`;
         if (node.subNodes) {
-            indexMd += generateIndexForProject(node.subNodes, prefix + "  ", directory, relativeDirectory);
+            indexMd += generateIndexForProject(node.subNodes, prefix + "  ", directory, relativeDirectory, collectedNodes);
         }
     }
     return indexMd;
+}
+
+function generateMarkdown(outputPath: string, node: DocTreeNode): void {
+    console.log(`Generating ${node.file!} -> ${outputPath}`);
 }
 
 export function convertDocumentToMarkdown(docTree: DocTree, directory: string): void {
@@ -23,13 +29,18 @@ export function convertDocumentToMarkdown(docTree: DocTree, directory: string): 
     const manualDir = path.join(directory, "manual");
     fs.mkdirSync(manualDir);
 
+    const collectedNodes: { [path: string]: DocTreeNode } = {};
     let indexMd = "# Manual\r\n\r\n";
     for (const projectNode of docTree.root.subNodes!) {
         if (projectNode.name === "Gaclib Document") continue;
         const subNodes = (projectNode.subNodes || []).filter(node => node.kind === "article" && node.name !== "Breaking changes from 1.0" && node.name !== "References");
         if (subNodes.length === 0) continue;
 
-        indexMd += `## ${projectNode.name}\r\n\r\n${generateIndexForProject(subNodes, "", manualDir, "./manual/")}\r\n`;
+        indexMd += `## ${projectNode.name}\r\n\r\n${generateIndexForProject(subNodes, "", manualDir, "./manual/", collectedNodes)}\r\n`;
+    }
+
+    for (const path in collectedNodes) {
+        generateMarkdown(path, collectedNodes[path]);
     }
 
     fs.writeFileSync(path.join(directory, "index.md"), indexMd, { encoding: 'utf-8' });
